5676d341207d3582d561024964ad54badb018bbd
Arin V Jain <arin.vdj@gmail.com>
2026-07-13 18:36:05 +0800

    Time: 0 ms (100%), Space: 54 MB (16.49%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let dp = Array(n+1).fill(-1);
+    function solve(n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = solve(n-1) + solve(n-2);
+        return dp[n];
+    }
+    return solve(n);
+};
