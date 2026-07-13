9ae5a3d663a64d2064f709713afe4e5b938027a4
Arin V Jain <arin.vdj@gmail.com>
2026-07-14 00:56:10 +0800

    Time: 1 ms (48.74%), Space: 57.7 MB (62.18%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(arr) {
+    let j = 1;
+    for (let i = 0 ; i < arr.length - 1; i++){
+        if(arr[i] != arr[i+1]){
+            arr[j] = arr[i+1];
+            j++;
+        }
+    }
+    return j;
+};
