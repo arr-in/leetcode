88c242eb8311d610c8124e5f498f287b0464cd4e
Arin V Jain <arin.vdj@gmail.com>
2026-07-15 04:42:21 +0800

    Time: 0 ms (100%), Space: 52.6 MB (93.92%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -3,8 +3,7 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-
-    if(digits.length === 0) return [];
+    if (digits.length == 0) return [];
     const map = {
         '2': 'abc',
         '3': 'def',
@@ -16,14 +15,14 @@ var letterCombinations = function(digits) {
         '9': 'wxyz'
     };
     let ans = [];
-    function solve(index, current){
-        if(index === digits.length){
+    function solve(index, current) {
+        if (index == digits.length) {
             ans.push(current);
             return;
         }
-        let ch = map[digits[index]];
-        for(let i = 0; i < ch.length; i++){
-            solve(index + 1, current + ch[i]);
+        let letters = map[digits[index]];
+        for (let i = 0; i < letters.length; i++) {
+            solve(index + 1, current + letters[i]);
         }
     }
     solve(0, "");
