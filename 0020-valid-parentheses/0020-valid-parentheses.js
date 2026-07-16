365e53abc4410d92e7f18343b501f0a0b52b1f61
Arin V Jain <arin.vdj@gmail.com>
2026-07-16 21:37:00 +0800

    Time: 4 ms (51.05%), Space: 55.1 MB (72.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        }
+        else {
+            if (stack.length === 0) {
+                return false;
+            }
+            let top = stack[stack.length - 1];
+            if ( (s[i] === ')' && top !== '(') || (s[i] === '}' && top !== '{') || (s[i] === ']' && top !== '[')) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.length === 0;
+};
