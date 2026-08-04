000f1eae7c766943fec694a07882d218e75b4b5b
Arin V Jain <arin.vdj@gmail.com>
2026-08-04 21:25:07 +0530

    Time: 2727 ms (5.28%), Space: 56.2 MB (75.58%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
+ */
+var numSubarraysWithSum = function(nums, goal) {
+    let count = 0;
+    for(let i = 0 ; i < nums.length ; i++){
+        let sum = 0;
+        for(let j = i ; j < nums.length ; j++){
+            sum = sum + nums[j];
+            if(sum === goal){
+                count++;
+            }
+        }
+    }
+    return count;
+};
