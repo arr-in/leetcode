9a2b3d5e4a3e5a93a28a866c358c24d93fd4be5a
Arin V Jain <arin.vdj@gmail.com>
2026-08-12 00:15:58 +0530

    Time: 2 ms (77.75%), Space: 64.7 MB (42.31%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0;
+    let ten = 0;
+    for(let i = 0 ; i < bills.length ; i++){
+        if(bills[i] === 5){
+            five++;
+        }else if(bills[i] === 10){
+            if(five <= 0){
+                return false;
+            }else{
+                five--;
+                ten++;
+            }
+        }else{
+            if(ten > 0 && five > 0) {
+                ten--;
+                five--;
+            }
+            else if(five >= 3) {
+                five -= 3;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return true;
+};
