9681c2cd7039df9448463f3401425acf9e325fb1
Arin V Jain <arin.vdj@gmail.com>
2026-08-14 04:56:36 +0530

    Time: 18 ms (17.37%), Space: 62.8 MB (35.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -4,18 +4,16 @@
  */
 var nextGreaterElements = function(nums) {
     let stack = [];
+    let ans = [];
     let n = nums.length;
-    let ans = new Array(n);
     for(let i = 2*n - 1 ; i >= 0 ; i--){
         while(stack.length > 0 && stack[stack.length-1] <= nums[i%n]){
             stack.pop();
         }
-        if(i < n){
-            if(stack.length === 0){
-                ans[i] = -1
-            }else{
-                ans[i] = stack[stack.length-1];
-            }
+        if(stack.length === 0){
+            ans[i%n] = -1; 
+        }else{
+            ans[i%n] = stack[stack.length-1];
         }
         stack.push(nums[i%n]);
     }
