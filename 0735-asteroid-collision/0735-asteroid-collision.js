8c478ae75d81bb9bdfec4377f217fde235cc1cca
Arin V Jain <arin.vdj@gmail.com>
2026-08-14 18:54:43 +0530

    Time: 5 ms (51.75%), Space: 55.3 MB (96.79%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -6,15 +6,16 @@ var asteroidCollision = function(asteroids) {
     let stack = [];
     for(let asteroid of asteroids){
         while(stack.length > 0 && stack[stack.length-1] > 0 && asteroid < 0){
-            let sum = stack[stack.length-1] + asteroid;
-            if(sum > 0){
-                asteroid = 0;
-            }else if(sum == 0){
-                asteroid = 0;
+            // collision will happen
+            let sum = asteroid + stack[stack.length-1];
+            if(sum < 0){
                 stack.pop();
+            }else if(sum > 0){
+                asteroid = 0;
             }else{
                 stack.pop();
-            }   
+                asteroid = 0;
+            }
         }
         if(asteroid !== 0){
             stack.push(asteroid);
