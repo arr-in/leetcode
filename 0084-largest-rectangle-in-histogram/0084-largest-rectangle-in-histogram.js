47b3e0d235a6a6fa5e13a4a38a4f5fc368ae5408
Arin V Jain <arin.vdj@gmail.com>
2026-08-14 18:26:16 +0530

    Time: 73 ms (12.78%), Space: 75.4 MB (23.44%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,42 +3,47 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    let stack = [];
-    let right = new Array(heights.length);  
-    let left = new Array(heights.length);
-
-    // left smaller element 
-    for(let i = 0 ; i < heights.length ; i++){
-        while(stack.length > 0 && heights[stack[stack.length-1]] >= heights[i]){
-            stack.pop();
+    let n = heights.length;
+    function pse(heights){
+        let left = [];
+        let stack = [];
+        for(let i = 0 ; i < n ; i++){
+            while(stack.length > 0 && heights[stack[stack.length-1]] >= heights[i]){
+                stack.pop();
+            }
+            if(stack.length === 0){
+                left[i] = -1;
+            }else{
+                left[i] = stack[stack.length-1];
+            }
+            stack.push(i);
         }
-        if(stack.length === 0){
-            left[i] = -1;
-        }else{
-            left[i] = stack[stack.length-1];
-        }
-        stack.push(i);
+        return left;
     }
-    while(stack.length > 0){
-        stack.pop();
-    }
-    // right smaller element
-    for(let i = heights.length-1 ; i >= 0 ; i--){
-        while(stack.length > 0 && heights[stack[stack.length-1]] >= heights[i]){
-            stack.pop();
+    function nse(heights){
+        let right = [];
+        let stack = [];
+        for(let i = n-1 ; i >= 0 ; i--){
+            while(stack.length > 0 && heights[stack[stack.length-1]] >= heights[i]){
+                stack.pop()
+            }
+            if(stack.length === 0){
+                right[i] = n;
+            }else{
+                right[i] = stack[stack.length-1];
+            }
+            stack.push(i);
         }
-        if(stack.length === 0){
-            right[i] = heights.length;
-        }else{
-            right[i] = stack[stack.length-1];
-        }
-        stack.push(i);
+        return right;
     }
+    let lse = pse(heights);
+    let rse = nse(heights);
     let maxarea = -Infinity;
-    for(let i = 0 ; i < heights.length ; i++){
-        let width = right[i] - left[i] - 1;
-        let area = heights[i] * width;
-        maxarea = Math.max(area, maxarea);
+    for(let i = 0 ; i < n ; i++){
+        let width = rse[i] - lse[i] - 1;
+        let height = heights[i];
+        let area = width * height;
+        maxarea = Math.max(maxarea , area);
     }
     return maxarea;
 };
