0b8c82cb06b2132fbaf4f288969811a31e212616
Arin V Jain <arin.vdj@gmail.com>
2026-09-18 00:36:06 +0530

    Time: 2 ms (65.86%), Space: 53.9 MB (92.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let farthest = 0;
     let jump = 0;
+    let farthest = 0;
     let end = 0;
     for(let i = 0 ; i < nums.length-1 ; i++){
-        farthest = Math.max(farthest , i + nums[i]);
+        farthest = Math.max(farthest , i+nums[i]);
         if(i === end){
             jump++;
             end = farthest;
