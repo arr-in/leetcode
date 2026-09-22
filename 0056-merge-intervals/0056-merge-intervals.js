9dfd07cc0520c75aec69e7159f3962fb524d9116
Arin V Jain <arin.vdj@gmail.com>
2026-09-22 23:18:08 +0530

    Time: 10 ms (40.26%), Space: 64.4 MB (69.58%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -1,22 +1,17 @@
-/**
- * @param {number[][]} intervals
- * @return {number[][]}
- */
 var merge = function(intervals) {
-    intervals.sort((a, b) => a[0] - b[0]);
     let result = [];
-    let i = 0;
-    while (i < intervals.length) {
-        let merge = intervals[i];
-        i++;
-        while (i < intervals.length && merge[1] >= intervals[i][0]) {
-            merge = [
-                Math.min(merge[0], intervals[i][0]),
-                Math.max(merge[1], intervals[i][1])
+    intervals.sort((a,b) => a[0] - b[0]);
+
+    for(let i = 0; i < intervals.length; i++) {
+        if(result.length === 0 || result[result.length - 1][1] < intervals[i][0]) {
+            result.push(intervals[i]);
+        } else {
+            result[result.length - 1] = [
+                Math.min(result[result.length - 1][0], intervals[i][0]),
+                Math.max(result[result.length - 1][1], intervals[i][1])
             ];
-            i++;
         }
-        result.push(merge);
     }
+
     return result;
 };
